af57ee2811f65020dc4d9cc933c5ee316923bc0b
ethankug <Ethanwkugler@gmail.com>
2020-04-04 22:36:59 +0000

    site finished

---
+++ my-app/build/precache-manifest.9e3957aa56b657776508735640a63ce2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba263551ff381f452c1599304b0f324e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "812e25d62844983219cb",
+    "url": "/static/css/main.bbe51d51.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812e25d62844983219cb",
+    "url": "/static/js/main.df47bfe0.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "b0e7416fce70f6be0399ad3806abf143",
+    "url": "/static/media/f0070229-coronavirus_artwork-spl.b0e7416f.svg"
+  }
+]);
---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './f0070229-coronavirus_artwork-spl.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="" alt="logo" />
+        <p>
+          Ethan Kugler
+        </p>
+        <a
+          className="App-link"
+          href="https://www.worldometers.info/coronavirus/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Live Data on COVID-19 Spread
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
